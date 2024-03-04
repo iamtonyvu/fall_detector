104dc944225fd24df2f7f13fdcde1f24fc7a6fbd
Yoshito Kono <4410kono@gmail.com>
2024-03-04 11:44:31 +0900

    add the alert to slack

--- fall_detector/api/fast.py
+++ fall_detector/api/fast.py
@@ -8,12 +8,15 @@ from fall_detector.params import *
 from fall_detector.utils.fall_detection import fall_detection_time
 from fall_detector.ml_logic.predict import detection, detection_json
 from fall_detector.utils.fall_detection import build_message
+from slack_sdk import WebhookClient
 
 app = FastAPI()
 
 model = YOLO(MODEL_PATCH)
 
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), MODEL_COMPRESSION]
+
+client = WebhookClient(os.environ["SLACK_WEBHOOK_URL"])
 
 async def detect(websocket: WebSocket, queue: asyncio.Queue):
     alert_send = False
@@ -39,6 +42,7 @@ async def detect_json(websocket: WebSocket, queue: asyncio.Queue):
         img, detections = detection_json(model, img, CLASS_NAMES, MODEL_CONFIDENCE, MODEL_CONFIDENCE_VISIBILITY)
         if alert_send == False and (falling_time := fall_detection_time(detections, falling_time)) == MODEL_TIME_ALERT:
             print(ALERT)
+            response = client.send(text='Your grandmother fell down!')
             await websocket.send_json(build_message('alert', [ALERT]))
             alert_send = True
         await websocket.send_json(build_message('message', list(detections.values())))
